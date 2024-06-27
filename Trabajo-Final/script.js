2226c9b717cd766cd189a901ff797a23d60b6ee4
Morena Aguero <155842105+MorenaAguero@users.noreply.github.com>
2024-06-26 22:54:15 -0300

    Add files via upload

---
+++ Trabajo-Final/script.js
@@ -0,0 +1,118 @@
+const searchEl = document.getElementById("search-input");
+const formEl = document.getElementById("search-form");
+const paginationsEl = document.querySelector('.paginations ul');
+const containerEl = document.getElementById("movies-container");
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=d576d4ab78c12fbc842e4fa4c93011f1&language=es-MX&page=`;
+const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=d576d4ab78c12fbc842e4fa4c93011f1&language=es-MX&query=`;
+
+let currentPage = parseInt(localStorage.getItem('currentPage')) || 1;
+const totalPages = 100; // Asumiendo 100 páginas para este ejemplo
+
+// Función para obtener películas
+const getMovies = async (url) => {
+  try {
+    const respuesta = await fetch(url);
+    if (!respuesta.ok) {
+      console.error("Error en la carga de datos");
+      return;
+    }
+    const datos = await respuesta.json();
+    // Se construye el HTML de las películas
+    let peliculas = "";
+    datos.results.forEach((pelicula) => {
+      peliculas += `
+      <a class="enlace" href="detalle_pelicula.html?id=${pelicula.id}">
+      <div class="pelicula">
+        <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+        <h3 class="titulo">${pelicula.title}</h3>
+      </div>
+      </a>
+      `;
+    });
+    containerEl.innerHTML = peliculas;
+    // Actualizar página activa
+    updateActivePage();
+    updatePagination();
+  } catch {
+    console.error("Error");
+  }
+};
+
+// Actualizar página activa
+const updateActivePage = () => {
+  const pages = paginationsEl.querySelectorAll('li[data-page]');
+  pages.forEach(page => {
+    if (parseInt(page.getAttribute("data-page")) === currentPage) {
+      page.classList.add("active");
+    } else {
+      page.classList.remove("active");
+    }
+  });
+};
+
+// Actualizar paginación
+const updatePagination = () => {
+  paginationsEl.innerHTML = '';
+
+  const startPage = currentPage > 5 ? currentPage - 4 : 1;
+  const endPage = currentPage > 5 ? currentPage + 1 : 5 ;
+
+  if (currentPage > 1) {
+    const prevLi = document.createElement('li');
+    prevLi.id = 'prev-btn';
+    prevLi.innerText = 'Anterior';
+    paginationsEl.appendChild(prevLi);
+    prevLi.addEventListener('click', () => {
+      if (currentPage > 1) {
+        currentPage--;
+        updatePageAndFetchMovies();
+      }
+    });
+  }
+
+  for (let i = startPage; i <= endPage && i <= totalPages; i++) {
+    const li = document.createElement('li');
+    li.setAttribute('data-page', i);
+    li.innerText = i;
+    if (i === currentPage) {
+      li.classList.add('active');
+    }
+    paginationsEl.appendChild(li);
+    li.addEventListener('click', () => {
+      currentPage = i;
+      updatePageAndFetchMovies();
+    });
+  }
+
+  if (currentPage < totalPages) {
+    const nextLi = document.createElement('li');
+    nextLi.id = 'next-btn';
+    nextLi.innerText = 'Siguiente';
+    paginationsEl.appendChild(nextLi);
+    nextLi.addEventListener('click', () => {
+      currentPage++;
+      updatePageAndFetchMovies();
+    });
+  }
+};
+
+// Actualizar página y obtener películas
+const updatePageAndFetchMovies = () => {
+  localStorage.setItem('currentPage', currentPage);
+  getMovies(API_URL + currentPage);
+};
+
+// Evento del formulario de búsqueda
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const searchTerm = searchEl.value;
+  if (searchTerm && searchTerm !== "") {
+    getMovies(searchURL + searchTerm);
+    searchEl.value = "";
+  } else {
+    window.location.reload();
+  }
+});
+
+// Carga inicial
+updatePageAndFetchMovies();
---
+++ Trabajo-Final/sinopsis.js
@@ -0,0 +1,36 @@
+const params = new URLSearchParams(window.location.search);
+const movieId = params.get('id');
+const containerEl = document.getElementById("container");
+const API_URL = `https://api.themoviedb.org/3/movie/${movieId}?api_key=d576d4ab78c12fbc842e4fa4c93011f1&language=es-MX`;
+const TRAILER_URL = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=d576d4ab78c12fbc842e4fa4c93011f1&language=es-MX`;
+
+const getData = async () => {
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        
+
+        const videoResponse = await fetch(TRAILER_URL);
+        const videoData = await videoResponse.json();
+        
+
+        // Busca el tráiler en los resultados
+        const trailer = videoData.results.find(video => video.type === 'Trailer' && video.site === 'YouTube');
+
+        const pelicula = `
+            <div class="pelicula">
+                <img class="poster" src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="Poster de ${data.title}">
+                <h3 class="titulo">${data.title}</h3>
+                <p class="sinopsis">${data.overview}</p>
+                <p class="fecha-lanzamiento">Fecha de lanzamiento: ${data.release_date}</p>
+                ${trailer ? `<iframe class="trailer" width="560" height="315" src="https://www.youtube.com/embed/${trailer.key}" frameborder="0" allowfullscreen></iframe>` : '<p>No se encontró tráiler.</p>'}
+            </div>
+        `;
+        containerEl.innerHTML = pelicula;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        containerEl.innerHTML = '<p>Hubo un error al cargar los datos de la película. Por favor, intenta nuevamente más tarde.</p>';
+    }
+};
+
+getData();
